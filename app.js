f844dc573920b465d6f883d2924d5f2ee0381c68
Tony Rizzotto <anthonyrizzotto@gmail.com>
2021-01-09 14:05:41 -0500

    Initial Commit
    Building a basic todo app that uses the browser as storage.

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * @description A function that will create a new todo-list and append the list to the dom.
+ */
+function createTodoList() {
+  //collect input from form
+  let newList = document.getElementById('new-tdl-list').value;
+
+  //create DOM elements for new TODO list
+  let tdlMenu = document.getElementById('todo-lists');
+
+  // Create a new list tag
+  let newLink = document.createElement('li');
+
+  // Create an anchor tag
+  let anchorTag = document.createElement('a');
+  anchorTag.setAttribute('href', '#');
+  anchorTag.innerText = newList;
+
+  //append anchor tag to a li tag
+  newLink.appendChild(anchorTag);
+
+  // append li tag to the UL
+  tdlMenu.appendChild(newLink);
+
+  return newLink;
+}
+
+// Event listener that runs the Create todo list
+document.getElementById('new-tdl-btn').addEventListener('click', function (e) {
+  //Prevent the default function
+  e.preventDefault();
+  // Create Todo
+  createTodoList();
+  //add and remove hidden class
+  document.getElementById('new-tdl-form').classList.add('hidden');
+  document.getElementById('new-todo-form').classList.remove('hidden');
+});
